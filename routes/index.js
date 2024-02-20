e0abc7624600a4e613807f680386ed6e92a5e167
Aryan Nayak <71493491+Aryan9131@users.noreply.github.com>
2024-02-20 13:17:59 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,6 @@
 const express=require('express');
 const router=express.Router();
 
-router.get('/', function(req, res){
-    console.log("Namaskar")
-    return res.status(200).json({
-        message:"Hello-1"
-    });
-})
-
 router.use('/questions', require('./questions'));
 router.use('/options', require('./options'));
 
