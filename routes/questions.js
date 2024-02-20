98f6b3ee591d1499d221177691ef27e3f725e257
Aryan Nayak <71493491+Aryan9131@users.noreply.github.com>
2024-02-20 13:28:50 +0530

    Added home route

--- routes/questions.js
+++ routes/questions.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const questionController=require('../controllers/questionController');
 const optionController=require('../controllers/optionController');
 
-
+router.get('/', questionController.findAll);
 router.post('/create',questionController.createQues);
 router.post('/:id/options/create',optionController.createOpt);
 router.get('/:id',questionController.findQues);
